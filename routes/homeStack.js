eb9987a7b43f48c1d5761c1abc36de954472095e
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-09-16 16:34:55 -0400

    added new screens

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,13 +1,17 @@
 import { createStackNavigator } from "react-navigation-stack";
 import Home from '../screens/home';
 import points1 from '../screens/points1';
-import points2 from '../screens/points2'
+import points2 from '../screens/points2';
+import Cam from '../screens/camera';
 import { createAppContainer } from "react-navigation";
 
 
 const screens = {
     Home: {
         screen: Home
+    },
+    Cam: {
+        screen: Cam
     },
     Points1: {
         screen: points1

75a9ea8b3380b24e257d21b06e6791adfab5813a
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-09-16 17:00:52 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import Navigator from './routes/homeStack';

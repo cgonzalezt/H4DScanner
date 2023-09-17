2583bbedd2070e8f92fb130630724c2a9d553c3a
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-09-17 05:11:24 -0400

    Update points1.js

--- screens/points1.js
+++ screens/points1.js
@@ -21,19 +21,34 @@ export default function Points1() {
              Get 15% off your next McDonalds order: 100pts
             </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Linking.openURL('https://www.mcdonalds.com/us/en-us.html')} style={styles2.ads}>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.pfchangs.com/')} style={styles2.ads}>
             <Text style={styles2.baseText}>
              Get 10% off your next P.F. Changs order: 125pts
             </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Linking.openURL('https://www.mcdonalds.com/us/en-us.html')} style={styles2.ads}>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.redlobster.com/')} style={styles2.ads}>
             <Text style={styles2.baseText}>
-             Get 25% off your next Buffalo Wild Wings order: 175pts
+             Get 25% off your next Red Lobster order: 175pts
             </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Linking.openURL('https://www.mcdonalds.com/us/en-us.html')} style={styles2.ads}>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.chipotle.com/')} style={styles2.ads}>
             <Text style={styles2.baseText}>
              Get 20% off your next Chipotle order: 150pts
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.dominos.com/en/')} style={styles2.ads}>
+            <Text style={styles2.baseText}>
+             Get 25% off your next Domino's order: 100pts
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.subway.com/en-us?utm_source=bing&utm_medium=cpc&utm_term=subway_exact&utm_content=brand&utm_campaign=&cid=0:0:00:0:nat-us:0&segment_code=0&&msclkid=d1f3026dfde912be45ec676b3fdff65d&gclid=d1f3026dfde912be45ec676b3fdff65d&gclsrc=3p.ds')} style={styles2.ads}>
+            <Text style={styles2.baseText}>
+             Get 10% off your next Subway order: 50pts
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL('https://www.starbucks.com/')} style={styles2.ads}>
+            <Text style={styles2.baseText}>
+             Get 30% off your next Starbucks order: 300pts
             </Text>
         </TouchableOpacity>
         </ScrollView>

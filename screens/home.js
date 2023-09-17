727a4f07ce794bbc6defbeca2594ded3192a0ff5
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-09-17 02:45:13 -0400

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -34,7 +34,7 @@ const Home = ({navigation}) => {
                 <Text style = {styles.baseText}>Dropoff Locations</Text>
             </TouchableOpacity>
 
-            <Image source={require=('../assets/leaf.png')} style= {styles.backgroundimage} />
+            <Image source={require=('../assets/leaf.png')} />
         </View>
 );
 };

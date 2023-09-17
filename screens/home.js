802edd6649a9607154f655e5b32cfa6ea40ed0c6
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-09-17 12:13:48 -0400

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -14,6 +14,7 @@ const Home = ({navigation}) => {
         <View style={styles.container}>
             <Image source={require('./../assets/pasto.jpeg')} style={styles.backgroundimage} />
             <Text style={styles.baseText}>
+                <Image source={require('./../assets/pengu.png')} style={styles.image}/>
             <Text style= {styles.titleText}>
                 {titleText}
             </Text>

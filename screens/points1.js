9d976b094ec40f89b2f0be82ec99675446c39b56
cgonzalezt <cristipu123@gmail.com>
2023-09-17 07:07:25 -0400

    Update points1.js

--- screens/points1.js
+++ screens/points1.js
@@ -23,49 +23,49 @@ export default function Points1() {
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.dominos.com/en/')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 25% off your next Domino's order: 75ts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.subway.com/en-us?utm_source=bing&utm_medium=cpc&utm_term=subway_exact&utm_content=brand&utm_campaign=&cid=0:0:00:0:nat-us:0&segment_code=0&&msclkid=d1f3026dfde912be45ec676b3fdff65d&gclid=d1f3026dfde912be45ec676b3fdff65d&gclsrc=3p.ds')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 10% off your next Subway order: 75pts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.pfchangs.com/')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 10% off your next P.F. Changs order: 150pts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.chipotle.com/')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 20% off your next Chipotle order: 150pts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.dunkindonuts.com/en')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 20% off your next Dunkin Donuts order: 150pts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.redlobster.com/')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 25% off your next Red Lobster order: 250pts
                     </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.starbucks.com/')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 30% off your next Starbucks order: 250pts
                     </Text>
                 </TouchableOpacity>
                 
-                <TouchableOpacity onPress={() => Linking.openURL('https://www.panerabread.com/en-us/home.html')} style={styles2.ads}>
+                <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                     <Text style={styles2.baseText}>
                      Get 25% off your next Panera Bread order: 250pts
                     </Text>
@@ -120,19 +120,86 @@ export default function Points1() {
                         </Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity onPress={() => Linking.openURL('https://www.redlobster.com/')} style={styles2.ads}>
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                         <Text style={styles2.baseText}>
                          Get 25% off your next Red Lobster order: 250pts
                         </Text>
                     </TouchableOpacity>
     
-                    <TouchableOpacity onPress={() => Linking.openURL('https://www.starbucks.com/')} style={styles2.ads}>
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                         <Text style={styles2.baseText}>
                          Get 30% off your next Starbucks order: 250pts
                         </Text>
                     </TouchableOpacity>
                     
-                    <TouchableOpacity onPress={() => Linking.openURL('https://www.panerabread.com/en-us/home.html')} style={styles2.ads}>
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 25% off your next Panera Bread order: 250pts
+                        </Text>
+                    </TouchableOpacity>
+                    </ScrollView>
+                </SafeAreaView>
+              );
+        } else if (getpointscounter() >= 150 && getpointscounter <= 250) {
+            return (
+                <SafeAreaView style={styles2.container}>
+                    <ScrollView style={styles2.scrollview}>
+                    <Text style= {styles2.titleText}>
+                        {titleText}
+                    </Text>
+            
+                    <Text style= {styles2.header}>
+                        {"Total number of points:  " + getpointscounter()}
+                    </Text>
+                    <TouchableOpacity onPress={() => Linking.openURL('https://www.mcdonalds.com/us/en-us.html')} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 15% off your next McDonalds order: 75pts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => Linking.openURL('https://www.dominos.com/en/')} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 25% off your next Domino's order: 75ts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => Linking.openURL('https://www.subway.com/en-us?utm_source=bing&utm_medium=cpc&utm_term=subway_exact&utm_content=brand&utm_campaign=&cid=0:0:00:0:nat-us:0&segment_code=0&&msclkid=d1f3026dfde912be45ec676b3fdff65d&gclid=d1f3026dfde912be45ec676b3fdff65d&gclsrc=3p.ds')} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 10% off your next Subway order: 75pts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 10% off your next P.F. Changs order: 150pts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 20% off your next Chipotle order: 150pts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 20% off your next Dunkin Donuts order: 150pts
+                        </Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 25% off your next Red Lobster order: 250pts
+                        </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
+                        <Text style={styles2.baseText}>
+                         Get 30% off your next Starbucks order: 250pts
+                        </Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity onPress={() => {<Text>Not enough points!</Text>}} style={styles2.ads}>
                         <Text style={styles2.baseText}>
                          Get 25% off your next Panera Bread order: 250pts
                         </Text>

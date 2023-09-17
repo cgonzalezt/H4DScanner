1c98a4d38bb12d0b327dfa9af077adb59014a44e
pardoluis123 <145076486+pardoluis123@users.noreply.github.com>
2023-09-17 04:55:18 -0400

    colors

--- screens/points1.js
+++ screens/points1.js
@@ -27,7 +27,7 @@ export default function Points1() {
 const styles2 = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightgreen',
+        backgroundColor: '#05C597',
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -37,7 +37,7 @@ const styles2 = StyleSheet.create({
     titleText: {
         fontSize: 60,
         fontWeight: 'bold',
-        color: 'darkgreen',
+        color: '#01FD70',
     },
     header: {
         fontSize: 30,
